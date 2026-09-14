cf88a6de00d039bf26b3bcdaaca54137569a4f00
teyonkim2026-prog <teyonkim2026@his.sc.kr>
2026-09-14 10:49:19 +0900

    Delete beach-weather-korea directory

--- beach-weather-korea/config.js
+++
@@ -1,6 +0,0 @@
-// 기상청 Open API 설정
-// 주의: GitHub Pages 같은 정적 웹사이트에서는 API 키를 브라우저에 넣어야 하므로
-// 공개 저장소에 올리면 키가 노출될 수 있습니다. 학교 프로젝트/개인 테스트용으로만 사용하세요.
-window.BEACH_CONFIG = {
-  serviceKey: "8MKRcxhpd3QRSobYoyUqi1QF6RVaer6JPIXQvcpUl2UQ4y%2F6RuwCoXCo8cHA%2Bj5k8e%2FsdpJAxK0lILuzEX2JWg%3D%3D"
-};
